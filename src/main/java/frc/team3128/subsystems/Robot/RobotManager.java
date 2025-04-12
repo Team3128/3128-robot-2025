d5d0a9eb968ba5ae3009892a45950c1428dd6c51
Spooky1000 <teja.yaramada@gmail.com>
2025-04-12 10:38:20 -0700

    Robot Manager Composed Auto Align Commands

--- src/main/java/frc/team3128/subsystems/Robot/RobotManager.java
+++ src/main/java/frc/team3128/subsystems/Robot/RobotManager.java
@@ -4,6 +4,8 @@ import common.core.fsm.FSMSubsystemBase;
 import common.core.fsm.TransitionMap;
 import common.utility.shuffleboard.NAR_Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Commands.*;
 import frc.team3128.subsystems.Swerve;
 import frc.team3128.subsystems.Climber.Climber;
 import frc.team3128.subsystems.Climber.ClimberStates;
@@ -14,17 +16,21 @@ import frc.team3128.subsystems.Intake.Intake;
 import frc.team3128.subsystems.Intake.IntakeStates;
 import frc.team3128.subsystems.Manipulator.Manipulator;
 import frc.team3128.subsystems.Manipulator.ManipulatorStates;
-
+import frc.team3128.Constants.FieldConstants.FieldStates;
+import static frc.team3128.Constants.FieldConstants.*;
 import static edu.wpi.first.wpilibj2.command.Commands.*;
 import static frc.team3128.subsystems.Robot.RobotStates.*;
 import edu.wpi.first.wpilibj.DriverStation;
 
+import java.util.List;
 import java.util.function.BooleanSupplier;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 
 public class RobotManager extends FSMSubsystemBase<RobotStates> {
     private static RobotManager instance;
@@ -73,20 +79,22 @@ public class RobotManager extends FSMSubsystemBase<RobotStates> {
             runOnce(()-> swerve.setThrottle(nextState.getThrottle())).unless(()-> DriverStation.isAutonomous() || Swerve.autoMoveEnabled)
         );
     }
-    public Command alignScoreCoral(boolean isRight, BooleanSupplier shouldRam){
+    public Command alignScoreCoral(boolean isRight){
+        final List<Pose2d> setpoints = isRight ? FieldStates.reefRight.asJava() : FieldStates.reefLeft.asJava();
+        Supplier<Pose2d> pose = () -> swerve.nearestPose2d(allianceFlip(setpoints));
         return parallel(
-            swerve.autoAlign(isRight, shouldRam).beforeStarting(()-> delayTransition = true), // do normal ram
+            swerve.navigateTo(pose),
             sequence(
                 waitUntil(()-> swerve.atElevatorDist()), // wait until safe for elevator to move
-                runOnce(()-> delayTransition = false),
-                runOnce(()-> {
+                Commands.runOnce(()-> delayTransition = false),
+                Commands.runOnce(()-> {
                     for(Pair<RobotStates, RobotStates> coupledState : coupledStates){
                         if (coupledState.getFirst() == getState()) {
                             sequence(
                                 waitUntil(() -> ElevatorMechanism.getInstance().atSetpoint()),
                                 waitUntil(()-> !Swerve.autoMoveEnabled),
                                 setStateCommand(coupledState.getSecond()),
-                                waitSeconds(0.5),
+                                waitSeconds(1),
                                 setStateCommand(NEUTRAL)
                             ).schedule();
                         }
@@ -95,6 +103,48 @@ public class RobotManager extends FSMSubsystemBase<RobotStates> {
             )
         );
     }
+
+    public Command alignAlgaeIntake() {
+        final List<Pose2d> setpoints = FieldStates.algaePoses.asJava();
+        Supplier<Pose2d> pose = ()-> swerve.nearestPose2d(allianceFlip(setpoints));
+        return parallel(
+            swerve.navigateTo(pose),
+            Commands.runOnce(
+                ()-> {
+                    if(FieldStates.idOf(pose.get()) % 2 == 0) setStateCommand(RSA2).schedule();
+                    else setStateCommand(RSA1).schedule();
+                }
+            )
+        );
+    }
+
+    public Command alignCoralIntake() {
+        final List<Pose2d> setpoints = List.of(FieldStates.SOURCE_LEFT.getPose2d(), FieldStates.SOURCE_RIGHT.getPose2d());
+        Supplier<Pose2d> pose = ()-> swerve.nearestPose2d(allianceFlip(setpoints));
+        return parallel(
+            swerve.navigateTo(pose),
+            setStateCommand(NEUTRAL)
+        );
+    }
+
+    public Command alignAlgaeScore() {
+        Supplier<Pose2d> pose = ()-> allianceFlip(new Pose2d(new Translation2d(7.7, swerve.getPose().getY()), Rotation2d.fromDegrees(0)));
+        return parallel(
+            swerve.navigateTo(pose),
+            swerve.navigateTo(pose),
+            sequence(
+                waitUntil(()-> swerve.atElevatorDist()), // wait until safe for elevator to move
+                Commands.runOnce(()-> delayTransition = false),
+                waitUntil(() -> ElevatorMechanism.getInstance().atSetpoint()),
+                waitUntil(()-> !Swerve.autoMoveEnabled),
+                setStateCommand(RSB),
+                waitSeconds(1),
+                setStateCommand(NEUTRAL)
+            )
+        );
+    }
+ 
+
     public void autoScore() {
         if (getState() == RobotStates.NEUTRAL || getState() == RobotStates.FULL_NEUTRAL)
             return;
