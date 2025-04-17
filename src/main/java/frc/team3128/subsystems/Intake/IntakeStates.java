b7bf032bc1fb45e2e6b3df415dac8619d9da7508
Team 3128: Controls <35188310+ControlsNarwhal@users.noreply.github.com>
2025-04-17 05:50:54 -0700

    switch to algae intake

--- src/main/java/frc/team3128/subsystems/Intake/IntakeStates.java
+++ src/main/java/frc/team3128/subsystems/Intake/IntakeStates.java
@@ -1,54 +1,3 @@
-package frc.team3128.subsystems.Intake;
-
-import common.hardware.motorcontroller.NAR_Motor.Neutral;
-import edu.wpi.first.math.Pair;
-import io.vavr.collection.List;
-
-public enum IntakeStates {
-    UNDEFINED,
-    NEUTRAL(-10, -.05),
-    INTAKE(120, -0.8),
-    EJECT_OUTTAKE(19, 0.7),
-    CLIMB_PRIME(-10, -.05);
-    
-    public static final List<IntakeStates> defaultStates = List.of(NEUTRAL, INTAKE, EJECT_OUTTAKE, CLIMB_PRIME);
-
-    private double angle;
-    private double power;
-    private Neutral neutral;
-
-    private IntakeStates(double angle, double power) {
-        this.angle = angle;
-        this.power = power;
-        this.neutral = Neutral.BRAKE;
-    }
-
-    private IntakeStates(double angle) {
-        this(angle, 0);
-    }
-
-    private IntakeStates() {
-        this.angle = 0;
-        this.power = 0;
-        this.neutral = Neutral.COAST;
-    }
-
-    public double getAngle() {
-        return this.angle;
-    }
-
-    public double getPower() {
-        return this.power;
-    }
-
-    public Neutral getNeutral() {
-        return this.neutral;
-    }
-}
-
-
-
-// BELOW IS ALGAE INTAKESTATES CODE
 // package frc.team3128.subsystems.Intake;
 
 // import common.hardware.motorcontroller.NAR_Motor.Neutral;
@@ -57,19 +6,12 @@ public enum IntakeStates {
 
 // public enum IntakeStates {
 //     UNDEFINED,
-//     NEUTRAL(0, 0.1),
-//     INTAKE(65, 0.8),
-//     EJECT_OUTTAKE(0, -0.8),
-//     HIGH_INTAKE(0, 0.8),
-//     CLIMB_PRIME(10),
-//     CLIMB(10);
-
-//     public static final List<IntakeStates> defaultStates = List.of(NEUTRAL, INTAKE, EJECT_OUTTAKE, CLIMB_PRIME, HIGH_INTAKE);
-//     public static final List<IntakeStates> exclusiveStates = List.of(CLIMB);
-
-//     public static final List<Pair<IntakeStates, IntakeStates>> coupledStates = List.of(
-//         Pair.of(CLIMB_PRIME, CLIMB)
-//     );
+//     NEUTRAL(-10, -.05),
+//     INTAKE(120, -0.8),
+//     EJECT_OUTTAKE(19, 0.7),
+//     CLIMB_PRIME(-10, -.05);
+    
+//     public static final List<IntakeStates> defaultStates = List.of(NEUTRAL, INTAKE, EJECT_OUTTAKE, CLIMB_PRIME);
 
 //     private double angle;
 //     private double power;
@@ -103,3 +45,61 @@ public enum IntakeStates {
 //         return this.neutral;
 //     }
 // }
+
+
+
+// BELOW IS ALGAE INTAKESTATES CODE
+package frc.team3128.subsystems.Intake;
+
+import common.hardware.motorcontroller.NAR_Motor.Neutral;
+import edu.wpi.first.math.Pair;
+import io.vavr.collection.List;
+
+public enum IntakeStates {
+    UNDEFINED,
+    NEUTRAL(0, 0.1),
+    INTAKE(65, 0.8),
+    EJECT_OUTTAKE(0, -0.8),
+    HIGH_INTAKE(0, 0.8),
+    CLIMB_PRIME(10),
+    CLIMB(10);
+
+    public static final List<IntakeStates> defaultStates = List.of(NEUTRAL, INTAKE, EJECT_OUTTAKE, CLIMB_PRIME, HIGH_INTAKE);
+    public static final List<IntakeStates> exclusiveStates = List.of(CLIMB);
+
+    public static final List<Pair<IntakeStates, IntakeStates>> coupledStates = List.of(
+        Pair.of(CLIMB_PRIME, CLIMB)
+    );
+
+    private double angle;
+    private double power;
+    private Neutral neutral;
+
+    private IntakeStates(double angle, double power) {
+        this.angle = angle;
+        this.power = power;
+        this.neutral = Neutral.BRAKE;
+    }
+
+    private IntakeStates(double angle) {
+        this(angle, 0);
+    }
+
+    private IntakeStates() {
+        this.angle = 0;
+        this.power = 0;
+        this.neutral = Neutral.COAST;
+    }
+
+    public double getAngle() {
+        return this.angle;
+    }
+
+    public double getPower() {
+        return this.power;
+    }
+
+    public Neutral getNeutral() {
+        return this.neutral;
+    }
+}
--- src/main/java/frc/team3128/subsystems/Intake/RollerMechanism.java
+++ src/main/java/frc/team3128/subsystems/Intake/RollerMechanism.java
@@ -1,66 +1,3 @@
-package frc.team3128.subsystems.Intake;
-
-import common.core.subsystems.VoltageSubsystemBase;
-import common.hardware.motorcontroller.NAR_CANSpark.ControllerType;
-import edu.wpi.first.wpilibj2.command.Command;
-import common.hardware.motorcontroller.NAR_CANSpark;
-import common.hardware.motorcontroller.NAR_Motor.MotorConfig;
-import common.hardware.motorcontroller.NAR_TalonFX;
-import common.utility.shuffleboard.NAR_Shuffleboard;
-
-import static frc.team3128.Constants.IntakeConstants.*;
-
-public class RollerMechanism extends VoltageSubsystemBase {
-
-    public static RollerMechanism instance;
-
-    protected static NAR_TalonFX leader = new NAR_TalonFX(ROLLER_LEADER_ID);
-
-    private RollerMechanism() {
-        super(leader);
-        // initShuffleboard();
-    }
-
-    public static RollerMechanism getInstance() {
-        if (instance == null) {
-            instance = new RollerMechanism();
-        }
-
-        return instance;
-    }
-
-    @Override
-    protected void configMotors() {
-        MotorConfig motorConfig = new MotorConfig(
-                ROLLER_GEAR_RATIO,
-                ROLLER_SAMPLE_PER_MINUTE,
-                ROLLER_STATOR_CURRENT_LIMIT,
-                ROLLER_INVERT,
-                ROLLER_NEUTRAL_MODE,
-                ROLLER_STATUS_FRAME);
-
-        leader.configMotor(motorConfig);
-    }
-
-    @Override
-    public boolean hasObjectPresent() {
-        return true;
-    }
-
-    @Override
-    public Command resetCommand() {
-        return null;
-    }
-
-    @Override
-    public void initShuffleboard() {
-        NAR_Shuffleboard.addData("Intake Current", "Current", () -> getCurrent(), 0, 0);
-    }
-}
-
-
-
-// BELOW IS ALGAE ROLLER CODE
 // package frc.team3128.subsystems.Intake;
 
 // import common.core.subsystems.VoltageSubsystemBase;
@@ -68,6 +5,7 @@ public class RollerMechanism extends VoltageSubsystemBase {
 // import edu.wpi.first.wpilibj2.command.Command;
 // import common.hardware.motorcontroller.NAR_CANSpark;
 // import common.hardware.motorcontroller.NAR_Motor.MotorConfig;
+// import common.hardware.motorcontroller.NAR_TalonFX;
 // import common.utility.shuffleboard.NAR_Shuffleboard;
 
 // import static frc.team3128.Constants.IntakeConstants.*;
@@ -76,7 +14,7 @@ public class RollerMechanism extends VoltageSubsystemBase {
 
 //     public static RollerMechanism instance;
 
-//     protected static NAR_CANSpark leader = new NAR_CANSpark(ROLLER_LEADER_ID, ControllerType.CAN_SPARK_FLEX);
+//     protected static NAR_TalonFX leader = new NAR_TalonFX(ROLLER_LEADER_ID);
 
 //     private RollerMechanism() {
 //         super(leader);
@@ -119,3 +57,65 @@ public class RollerMechanism extends VoltageSubsystemBase {
 //         NAR_Shuffleboard.addData("Intake Current", "Current", () -> getCurrent(), 0, 0);
 //     }
 // }
+
+
+
+// BELOW IS ALGAE ROLLER CODE
+package frc.team3128.subsystems.Intake;
+
+import common.core.subsystems.VoltageSubsystemBase;
+import common.hardware.motorcontroller.NAR_CANSpark.ControllerType;
+import edu.wpi.first.wpilibj2.command.Command;
+import common.hardware.motorcontroller.NAR_CANSpark;
+import common.hardware.motorcontroller.NAR_Motor.MotorConfig;
+import common.utility.shuffleboard.NAR_Shuffleboard;
+
+import static frc.team3128.Constants.IntakeConstants.*;
+
+public class RollerMechanism extends VoltageSubsystemBase {
+
+    public static RollerMechanism instance;
+
+    protected static NAR_CANSpark leader = new NAR_CANSpark(ROLLER_LEADER_ID, ControllerType.CAN_SPARK_FLEX);
+
+    private RollerMechanism() {
+        super(leader);
+        // initShuffleboard();
+    }
+
+    public static RollerMechanism getInstance() {
+        if (instance == null) {
+            instance = new RollerMechanism();
+        }
+
+        return instance;
+    }
+
+    @Override
+    protected void configMotors() {
+        MotorConfig motorConfig = new MotorConfig(
+                ROLLER_GEAR_RATIO,
+                ROLLER_SAMPLE_PER_MINUTE,
+                ROLLER_STATOR_CURRENT_LIMIT,
+                ROLLER_INVERT,
+                ROLLER_NEUTRAL_MODE,
+                ROLLER_STATUS_FRAME);
+
+        leader.configMotor(motorConfig);
+    }
+
+    @Override
+    public boolean hasObjectPresent() {
+        return true;
+    }
+
+    @Override
+    public Command resetCommand() {
+        return null;
+    }
+
+    @Override
+    public void initShuffleboard() {
+        NAR_Shuffleboard.addData("Intake Current", "Current", () -> getCurrent(), 0, 0);
+    }
+}
