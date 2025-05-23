c01ac282336802d55ee439ddebbfa992d8bee785
Team 3128: Controls <35188310+ControlsNarwhal@users.noreply.github.com>
2025-05-23 15:50:53 -0700

    led updated code

--- src/main/java/frc/team3128/subsystems/Leds/Leds.java
+++ src/main/java/frc/team3128/subsystems/Leds/Leds.java
@@ -1,13 +1,20 @@
 package frc.team3128.subsystems.Leds;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import common.core.fsm.FSMSubsystemBase;
 import common.core.fsm.TransitionMap;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.team3128.Constants.LedsConstants;
+import frc.team3128.Constants.FieldConstants.FieldStates;
+import frc.team3128.subsystems.Swerve;
 import frc.team3128.subsystems.Manipulator.ManipulatorStates;
+import static frc.team3128.Constants.FieldConstants.*;
 
 public class Leds extends FSMSubsystemBase<LedsStates> {
 
@@ -28,13 +35,22 @@ public class Leds extends FSMSubsystemBase<LedsStates> {
         return instance;
     }
 
-    public Leds() {
+    private Leds() {
         super(LedsStates.class, transitionMap, LedsStates.DISABLED);
     }
 
     @Override
     public void registerTransitions() {
-        // NEUTRAL, IN, OUT are able to transition between each other
         transitionMap.addCommutativeTransition(List.of(LedsStates.values()), defaultTransitioner);
+        transitionMap.addConvergingTransition(LedsStates.NEUTRAL, run(
+            () -> LedsMechanism.getInstance().setColor(LedsStates.NEUTRAL.getColor(), (int) (LedsConstants.MAX_HEIGHT * Math.max(1.0, 0.03 / getClosestDist())))
+        ).until(() -> this.currentState != LedsStates.NEUTRAL));
+    }
+
+    final List<Pose2d> coralSetpoints = FieldStates.reefPoses.asJava();
+    Supplier<Pose2d> pose = ()-> Swerve.getInstance().nearestPose2d(allianceFlip(coralSetpoints));
+
+    private double getClosestDist() {
+        return Swerve.getInstance().getPose().minus(pose.get()).getTranslation().getNorm();
     }
 }
