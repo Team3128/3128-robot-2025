7731b4b0f2c7ca94a1c204d5e0f22303e76a92b9
Lucas Han <50851263+bloobglob@users.noreply.github.com>
2025-10-22 15:24:36 -0700

    set up for elevator sysid

--- src/main/java/frc/team3128/RobotContainer.java
+++ src/main/java/frc/team3128/RobotContainer.java
@@ -54,6 +54,7 @@ import frc.team3128.subsystems.Manipulator.Manipulator;
 import frc.team3128.subsystems.Robot.RobotManager;
 import frc.team3128.subsystems.Robot.RobotStates;
 import edu.wpi.first.math.Pair;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
@@ -171,13 +172,19 @@ public class RobotContainer {
         // buttonPad.getButton(8).onTrue(runOnce(()-> LedsMechanism.getInstance().setColor(Color.kBlue)));
 
 
-        controller2.getButton(kA).onTrue(WinchMechanism.getInstance().runCommand(0.5)).onFalse(WinchMechanism.getInstance().stopCommand());
-        controller2.getButton(kB).onTrue(WinchMechanism.getInstance().runCommand(-0.5)).onFalse(WinchMechanism.getInstance().stopCommand());
+        // controller2.getButton(kA).onTrue(WinchMechanism.getInstance().runCommand(0.5)).onFalse(WinchMechanism.getInstance().stopCommand());
+        // controller2.getButton(kB).onTrue(WinchMechanism.getInstance().runCommand(-0.5)).onFalse(WinchMechanism.getInstance().stopCommand());
         controller2.getButton(kX).onTrue(WinchMechanism.getInstance().resetCommand().ignoringDisable(true));
         controller2.getButton(kY).onTrue(robot.setStateCommand(FULL_NEUTRAL));
         
         controller2.getButton(kStart).onTrue(Commands.runOnce(()->LedsMechanism.getInstance().setColor(Color.kBlue)));
 
+        controller2.getButton(kA).onTrue(ElevatorMechanism.getInstance().runCommand(-0.5)).onFalse(ElevatorMechanism.getInstance().stopCommand());
+        controller2.getButton(kB).onTrue(ElevatorMechanism.getInstance().runCommand(0.5)).onFalse(ElevatorMechanism.getInstance().stopCommand());
+        controller2.getButton(kLeftTrigger).whileTrue(ElevatorMechanism.getInstance().sysIdDynamic(Direction.kReverse));
+        controller2.getButton(kLeftBumper).whileTrue(ElevatorMechanism.getInstance().sysIdDynamic(Direction.kForward));
+        controller2.getButton(kRightTrigger).whileTrue(ElevatorMechanism.getInstance().sysIdQuasistatic(Direction.kReverse));
+        controller2.getButton(kRightBumper).whileTrue(ElevatorMechanism.getInstance().sysIdQuasistatic(Direction.kForward));
 
         controller.getButton(kA).onTrue(robot.getTempToggleCommand(RPL1, RSL1));
         controller.getButton(kB).onTrue(robot.getTempToggleCommand(RPL2, RSL2));
--- src/main/java/frc/team3128/subsystems/Elevator/ElevatorMechanism.java
+++ src/main/java/frc/team3128/subsystems/Elevator/ElevatorMechanism.java
@@ -6,6 +6,14 @@ import common.core.subsystems.PositionSubsystemBase;
 import common.hardware.motorcontroller.NAR_CANSpark.ControllerType;
 import common.hardware.motorcontroller.NAR_CANSpark;
 import common.hardware.motorcontroller.NAR_Motor.MotorConfig;
+import edu.wpi.first.units.measure.MutDistance;
+import edu.wpi.first.units.measure.MutLinearVelocity;
+import edu.wpi.first.units.measure.MutVoltage;
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+
+import static edu.wpi.first.units.Units.*;
 import static frc.team3128.Constants.ElevatorConstants.*;
 
 public class ElevatorMechanism extends PositionSubsystemBase {
@@ -49,6 +57,39 @@ public class ElevatorMechanism extends PositionSubsystemBase {
        controller.setInputRange(ELEVATOR_POSITION_MIN, ELEVATOR_POSITION_MAX);
        controller.configureFeedback(left);
        controller.setTolerance(ELEVATOR_TOLERANCE);
-       addDisableCondition(()-> ((getPosition() >= 0.90 * ELEVATOR_POSITION_MAX) && (getVolts() > 6) && (getVelocity() > 0))); // if too close to top and going towards top and getting too much power then disable
     }   
+
+    public SysIdRoutine driveRoutine = new SysIdRoutine(
+        new SysIdRoutine.Config(Volts.of(0.2).per(Second), Volts.of(4), null),
+        new SysIdRoutine.Mechanism((v) -> runVolts(v.in(Volts)), this::logMotors, this)
+    );
+
+    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+        if (direction == SysIdRoutine.Direction.kForward) {
+            return driveRoutine.quasistatic(direction).onlyWhile(() -> left.getPosition() < 0.8 * ELEVATOR_POSITION_MAX);
+        } else {
+            return driveRoutine.quasistatic(direction).onlyWhile(() -> left.getPosition() > 0.2 * ELEVATOR_POSITION_MAX);
+        }
+    }
+      
+    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+        if (direction == SysIdRoutine.Direction.kForward) {
+            return driveRoutine.dynamic(direction).onlyWhile(() -> left.getPosition() < 0.8 * ELEVATOR_POSITION_MAX);
+        } else {
+            return driveRoutine.dynamic(direction).onlyWhile(() -> left.getPosition() > 0.2 * ELEVATOR_POSITION_MAX);
+        }
+    }
+    
+    private final MutVoltage appliedVoltage = Volts.mutable(0);
+    private final MutDistance position = Meters.mutable(0);
+    private final MutLinearVelocity velocity = MetersPerSecond.mutable(0);
+    public void logMotors(SysIdRoutineLog log){
+        // log.motor("position").linearPosition(Meters.of(left.getPosition()));
+        // log.motor("velocity").linearVelocity(MetersPerSecond.of(left.getVelocity() / 60.0));
+        // log.motor("voltage").voltage(Volts.of(12 * left.getAppliedOutput()));
+        log.motor("elevator-motor")
+            .linearPosition(position.mut_replace(left.getPosition(), Meters))
+            .linearVelocity(velocity.mut_replace(left.getVelocity(), MetersPerSecond))
+            .voltage(appliedVoltage.mut_replace(left.getAppliedOutput() * 12, Volts));
+    }
 }
